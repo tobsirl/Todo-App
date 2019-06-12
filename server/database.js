6b1bfa29025a55d67e72f3c7c94b62a7d8b1afbd
Paul Tobin <tobster111@gmail.com>
2019-06-12 13:02:43 +0100

    Setup the database connect in database.js, cloud based Mongo Atlas

---
+++ server/database.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const { MONGOURI } = process.env;
+
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(MONGOURI, { useNewUrlParser: true });
+    console.log(`Connection to Database established...`);
+  } catch (err) {
+    console.error(err.message);
+    // Exit connection process
+    process.exit(1);
+  }
+};
+
+export default connectDatabase;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import 'dotenv/config';
 
+import connectDatabase from './database';
+
 const app = express();
 
 const { PORT } = process.env;
 
+// Make database connection
+connectDatabase();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}!`);
+  console.log(`Server listening on port ${PORT}! 🚀`);
 });
