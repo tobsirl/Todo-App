c00a3382102fb2da475767a4f1d7b009907d9a7a
Paul Tobin <tobster111@gmail.com>
2019-06-13 09:55:10 +0100

    Three conditions tested with postman, no token, incorrect token and correct token. All working

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,10 +1,21 @@
 import express from 'express';
 
+import auth from '../../middleware/auth';
+import User from '../../models/User';
+
 const router = express.Router();
 
 // @route   GET api/auth/test
 // @desc    Tests auth route
 // @access  Public
-router.get('/test', (req, res) => res.json({ msg: 'Auth Endpoint' }));
+router.get('/test', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import connectDatabase from './database';
 import users from './routes/api/users';
 import tasks from './routes/api/tasks';
+import auth from './routes/api/auth';
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(express.json({ extended: false })); // no need for bodyparser anymore
 // Use Routes
 app.use('/api/users', users);
 app.use('/api/tasks', tasks);
+app.use('/api/auth', auth);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}! 🚀`);
