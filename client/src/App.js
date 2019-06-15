47dad0126ee6b8429b8080cce45f28877614b136
Paul Tobin <tobster111@gmail.com>
2019-06-15 19:39:47 +0100

    Added a components folder > layout containing Navbar.js + Landing.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>App.js</h1>
-      </div>
+      <Fragment>
+        <Navbar />
+        <Landing />
+      </Fragment>
     );
   }
 }
