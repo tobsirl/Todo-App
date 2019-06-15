9f51f398b97c316373fcc4923cf97371c5253894
Paul Tobin <tobster111@gmail.com>
2019-06-15 22:23:16 +0100

    Added auth folder with Login + Register components, added routing to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 class App extends Component {
   render() {
@@ -11,6 +13,12 @@ class App extends Component {
         <Fragment>
           <Navbar />
           <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
         </Fragment>
       </Router>
     );
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'
+import './Navbar.css';
 
 const Navbar = () => {
   return (
@@ -10,7 +10,17 @@ const Navbar = () => {
           <i className="fas fa-tasks" /> Todo App
         </Link>
       </h1>
-   
+      <ul>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
